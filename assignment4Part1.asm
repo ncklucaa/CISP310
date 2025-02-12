d44896c96a7362fd96a9bdb7709345af31dd252a
Nick Luca <159816543+ncklucaa@users.noreply.github.com>
2025-02-12 13:53:38 -0800

    Update assignment4Part1.asm

--- assignment4Part1.asm
+++ assignment4Part1.asm
@@ -12,29 +12,29 @@ INCLUDE io.h            ; header file for input/output
 
 .DATA 
 numOfGallons	DWORD	?
-numOfMiles		DWORD	?
-prompt1			BYTE	"Enter the number of gallons of gas the car can hold:", 0
-prompt2			BYTE	"Number of miles it can be driven on a full tank:",	0
-resultLbl		BYTE	"Miles per gallon the car gets:", 0
-string			BYTE	40 DUP (?)
-mpg				BYTE	40 DUP (?)
+numOfMiles	DWORD	?
+prompt1		BYTE	"Enter the number of gallons of gas the car can hold:", 0
+prompt2		BYTE	"Number of miles it can be driven on a full tank:", 0
+resultLbl	BYTE	"Miles per gallon the car gets:", 0
+string		BYTE	40 DUP (?)
+mpg		BYTE	40 DUP (?)
 
 .CODE
 _MainProc	PROC
-			input	prompt1, string, 40     ; read ASCII characters
-			atod	string			; convert to integer
-			mov	numOfGallons, eax	; store in memory
+		input	prompt1, string, 40     ; read ASCII characters
+		atod	string			; convert to integer
+		mov	numOfGallons, eax	; store in memory
 
-			input	prompt2, string, 40	; repeat for second number
-			atod	string
-			mov	numOfMiles, eax 
+		input	prompt2, string, 40	; repeat for second number
+		atod	string
+		mov	numOfMiles, eax 
 
-			mov	eax, numOfMiles		; first number to EAX
-			cdq
-			idiv	numOfGallons		; divide
-			dtoa	mpg, eax		; convert to ASCII characters
-			output	resultLbl, mpg		; output label and mpg
-			mov		eax, 0		; exit with return code 0
-			ret
+		mov	eax, numOfMiles		; first number to EAX
+		cdq
+		idiv	numOfGallons		; divide
+		dtoa	mpg, eax		; convert to ASCII characters
+		output	resultLbl, mpg		; output label and mpg
+		mov		eax, 0		; exit with return code 0
+		ret
 _MainProc ENDP
-END							; end of source code
+END						; end of source code
